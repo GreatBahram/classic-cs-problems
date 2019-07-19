0b497427294f0b321bcab9db0d378b2380ee0c20
Bahram Aghaei <aghaee.bahram@gmail.com>
2019-07-19 16:30:05 +0430

    add calculating_pi

---
+++ chapter1/calculating_pi.py
@@ -0,0 +1,11 @@
+def calculate_pi(n_terms: int) -> float:
+    """ Calculate pi by using Leibniz formula."""
+    numerator: float = 4.0
+    denominator: float = 1.0
+    operation: float = 1.0
+    pi: float = 0.0
+    for _ in range(n_terms):
+        pi += operation * (numerator / denominator)
+        denominator += 2.0
+        operation *= -1.0
+    return pi
