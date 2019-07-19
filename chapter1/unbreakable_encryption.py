e3ecdf3652e8fe80c76800b5230d43a80347fa67
Bahram Aghaei <aghaee.bahram@gmail.com>
2019-07-19 13:15:32 +0430

    add unbreakable_encryption

---
+++ chapter1/unbreakable_encryption.py
@@ -0,0 +1,22 @@
+from secrets import token_bytes
+from typing import Tuple
+
+
+def random_key(length: int) -> int:
+    """ Generate some dummy data."""
+    tb = token_bytes(length)
+    return int.from_bytes(tb, "big")
+
+
+def encrypt(original: str) -> Tuple[int, int]:
+    original_bytes: bytes = original.encode()
+    dummy: int = random_key(len(original_bytes))
+    original_key: int = int.from_bytes(original_bytes, "big")
+    encrypted: int = original_key ^ dummy
+    return dummy, encrypted
+
+
+def decrypt(key1: int, key2: int) -> str:
+    decrypted: int = key1 ^ key2
+    temp: bytes = decrypted.to_bytes((decrypted.bit_length() + 7) // 8, "big")
+    return temp.decode()
